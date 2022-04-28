1cd0767984b3050dc47388f0de4c39365cc788e0
leonard33 <leonardkombo14@gmail.com>
2022-04-28 11:10:40 +0300

    fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/*const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -28,4 +28,5 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
-};
+};*/
+const path = require('path'); const HtmlWebpackPlugin = require('html-webpack-plugin'); module.exports = { mode: 'development', entry: './src/index.js', devtool: 'inline-source-map', devServer: { static: './dist', }, plugins: [ new HtmlWebpackPlugin({ template: './src/index.html', }), ], output: { filename: '[name].js', path: path.resolve(__dirname, 'dist'), clean: true, }, optimization: { runtimeChunk: 'single', }, module: { rules: [ { test: /\.css$/i, use: ['style-loader', 'css-loader'], }, { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', }, { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource', }, ], }, }; 
