c0d04a413d78da5730e4139819a8e41354044e25
leonard33 <leonardkombo14@gmail.com>
2022-05-02 12:41:26 +0300

    Add github pages deploy and configuring webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/project-todolist/',
     clean: true,
   },
   optimization: {
